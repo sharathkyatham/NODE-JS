9b801ead5ca43a0b59755f28ebfeb0b3593b44bd
Your Name <you@example.com>
2024-06-24 22:31:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        "Brand": "Bentley",
+        "Model": "Flying Spur",
+        "Engine": "W12 Twin-Turbo",
+        "Price": "7.60cr"
+    },
+    {
+        "Brand": "RollsRoyce",
+        "Model": "BoatTail",
+        "Engine": "V12 Twin-Turbo",
+        "Price": "233cr"
+    },
+    {
+        "Brand": "Aston Martin",
+        "Model": "DBS SuperLeggera",
+        "Engine": "V12",
+        "Price": "4.59cr"
+    },
+    {
+        "Brand": "Jaguar",
+        "Model": "XJL",
+        "Engine": "V8",
+        "Price": "2.1cr"
+    },
+];
+console.table(data);
+// console.log(data);
